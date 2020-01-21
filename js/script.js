a9643a9fd7b4f412b1f5cacaf85a347c5fd43cf3
saprtag <47823846+saprtag@users.noreply.github.com>
2020-01-21 23:13:30 +0300

    changing the script for links

--- js/script.js
+++ js/script.js
@@ -5,12 +5,10 @@ var link = document.getElementsByClassName("content__link")[0];
 
 vk_button.onclick = function () {
   link.classList.toggle("vk__link")
- // console.log("vk__link");
+  console.log("vk__link");
   vk_button.classList.toggle("inst__link")
+  var t = link.href;
+  link.href = link.dataset.href;
+  link.dataset.href = t;
+  console.log(link.href);
 }
-
-  vk_button.addEventListener('click', function () {
-    var $a = 'https://www.instagram.com/_sapr_/'
-    link.href = $a
-  //  console.log($a);
-  })
