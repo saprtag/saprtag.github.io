9c74c28bf91710227b9e11c8461ebecdf98ee6f8
saprtag <47823846+saprtag@users.noreply.github.com>
2020-01-21 17:46:01 +0300

    comment log

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ var link = document.getElementsByClassName("content__link")[0];
 
 vk_button.onclick = function () {
   link.classList.toggle("vk__link")
-  console.log("vk__link");
+ // console.log("vk__link");
   vk_button.classList.toggle("inst__link")
 }
 
   vk_button.addEventListener('click', function () {
     var $a = 'https://www.instagram.com/_sapr_/'
     link.href = $a
-    console.log($a);
+  //  console.log($a);
   })
